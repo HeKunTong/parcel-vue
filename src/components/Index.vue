ac68bac04673e3f6d531a5dd3fd702f7611fcf64
blank <1161709455@qq,com>
2018-10-16 10:04:58 +0800

    parcel-vue例子

---
+++ src/components/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>{{ name }}</div>
+</template>
+
+<script>
+    export default {
+        name: "Index",
+        data() {
+            return {
+                name: 'hello world'
+            };
+        }
+    }
+</script>
